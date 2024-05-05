67d311bb65221ebd800dee5956c11d25689ccda6
Sochniynapas <66013035+Sochniynapas@users.noreply.github.com>
2024-05-06 01:41:27 +0700

    Добавил картинку и svg записаться на основную область страницы

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,19 @@
 import "./Home.css"
-
+import image from "../Images/picture_1.png"
+import { SignUpButton } from "../Icons/icons"
 const Home = () => {
-  return <span className="title">СТУДИЯ ЙОГИ И ПИЛАТЕСА</span>
+  return (
+    <div className="home_view">
+      <div className="left_side">
+        <span className="title">СТУДИЯ ЙОГИ И ПИЛАТЕСА</span>
+        <span className="desc">
+          Здесь мы поможем вам держать тело в тонусе, а душу - в гармонии.
+        </span>
+        <SignUpButton />
+      </div>
+      <img className="home_big_image" src={image} alt=""></img>
+    </div>
+  )
 }
 
 export default Home
